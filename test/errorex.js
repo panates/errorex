fe3c70dadf578bad14b149e089ca08f4c1b5019d
Eray Hanoğlu <erayszn@gmx.net>
2018-10-08 15:21:38 +0300

    Initial release of v2.0

--- test/errorex.js
+++ test/errorex.js
@@ -1,128 +1,154 @@
 /* eslint-disable */
 const assert = require('assert');
-const erx = require('../');
+const path = require('path');
+const ErrorEx = require('../');
+const {
+  ArgumentError,
+  RangeError,
+  HttpError,
+  HttpClientError,
+  HttpServerError,
+  ValidateError
+} = ErrorEx;
 
 describe('ErrorEx', function() {
 
-  it('should create ErrorEx', function(done) {
-    var error = erx.ErrorEx('Error message. %s', 'ok');
+  it('should create ErrorEx', function() {
+    const error = new ErrorEx('Error message. %s', 'ok');
     assert.equal(error.message, 'Error message. ok');
     assert.equal(error.name, 'Error');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
+    assert.ok(error instanceof ErrorEx);
     assert.ok(error.stack);
-    done();
   });
 
-  it('should pass Error object as first argument', function(done) {
-    var e = new Error('Error message');
+  it('should inherit from other Error', function() {
+    const e = new Error('Error message');
+    e.name = 'AnyName';
     e.param1 = 123;
-    var error = erx.ErrorEx(e);
+    const error = new ErrorEx(e);
     assert.equal(error.message, 'Error message');
     assert.equal(error.name, 'Error');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
+    assert.ok(error instanceof ErrorEx);
     assert.equal(error.param1, 123);
     assert.ok(error.stack);
-    done();
   });
 
-  it('should set properties with ErrorEx.set(key, value)', function(done) {
-    var error = erx.ErrorEx('message');
-    error.set('prm1', 1);
-    error.setCode(12345);
-    error.setDetails('details');
+  it('should get stack without node internals', function() {
+    const error = new ErrorEx('Error message. %s', 'ok');
+    const s = error.getStack(true);
+    assert(!s.includes('(internal'));
+  });
+
+  it('should get stack frames', function() {
+    const error = new ErrorEx('Error message. %s', 'ok');
+    const s = error.getStackFrames();
+    assert(!s.includes('(internal'));
+  });
+
+  it('should get call stack', function() {
+    const calls = ErrorEx.getCallStack();
+    assert.equal(calls[0], path.resolve(__dirname, '../lib/errorex.js'));
+    assert.equal(calls[1], path.resolve(__filename));
+    assert(calls.length >= 5);
+  });
+
+  it('should get call stack without node internals', function() {
+    const calls = ErrorEx.getCallStack(true);
+    assert.equal(calls[0], path.resolve(__dirname, '../lib/errorex.js'));
+    assert.equal(calls[1], path.resolve(__filename));
+    assert(calls.length < 5);
+  });
+
+  it('should set properties with ErrorEx.set(key, value)', function() {
+    const error = new ErrorEx('message')
+        .set('prm1', 1)
+        .set('code', 12345)
+        .set('details', 'details');
     assert.equal(error.prm1, 1);
     assert.equal(error.code, 12345);
     assert.equal(error.details, 'details');
-    done();
   });
 
-  it('should set properties with ErrorEx.set(object)', function(done) {
-    var error = erx.ErrorEx('message');
-    error.set({
-      prm1: 1,
-      code: 12345,
-      details: 'details'
-    });
+  it('should set properties with ErrorEx.set(object)', function() {
+    const error = new ErrorEx('message')
+        .set({
+          prm1: 1,
+          code: 12345,
+          details: 'details'
+        });
     assert.equal(error.prm1, 1);
     assert.equal(error.code, 12345);
     assert.equal(error.details, 'details');
-    done();
   });
 
-  it('should create ArgumentError', function(done) {
-    var error = erx.ArgumentError('Error message. %s', 'ok');
-    error.setArgumentIndex(1);
-    error.setArgumentName('arg1');
+  it('should create ArgumentError', function() {
+    const error = new ArgumentError('Error message. %s', 'ok')
+        .setArgumentIndex(1)
+        .setArgumentName('arg1');
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.ArgumentError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof ArgumentError);
     assert.equal(error.name, 'ArgumentError');
     assert.equal(error.argumentIndex, 1);
     assert.equal(error.argumentName, 'arg1');
-    done();
   });
 
-  it('should create RangeError', function(done) {
-    var error = erx.RangeError('Error message. %s', 'ok');
-    error.setMinValue(1);
-    error.setMaxValue(5);
+  it('should create RangeError', function() {
+    const error = new RangeError('Error message. %s', 'ok')
+        .setMinValue(1)
+        .setMaxValue(5);
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.RangeError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof RangeError);
     assert.equal(error.name, 'RangeError');
     assert.equal(error.minValue, 1);
     assert.equal(error.maxValue, 5);
-    done();
   });
 
-  it('should create HttpError', function(done) {
-    var error = erx.HttpError('Error message. %s', 'ok');
-    error.setStatus(404);
+  it('should create HttpError', function() {
+    const error = new HttpError('Error message. %s', 'ok')
+        .setStatus(404);
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.HttpError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof HttpError);
     assert.equal(error.name, 'HttpError');
     assert.equal(error.status, 404);
-    done();
   });
 
-  it('should create HttpClientError', function(done) {
-    var error = erx.HttpClientError('Error message. %s', 'ok');
+  it('should create HttpClientError', function() {
+    const error = new HttpClientError('Error message. %s', 'ok');
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.HttpError);
-    assert.ok(error instanceof erx.HttpClientError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof HttpError);
+    assert.ok(error instanceof HttpClientError);
     assert.equal(error.name, 'HttpClientError');
     assert.equal(error.status, 400);
-    done();
   });
 
-  it('should create HttpServerError', function(done) {
-    var error = erx.HttpServerError('Error message. %s', 'ok');
+  it('should create HttpServerError', function() {
+    const error = new HttpServerError('Error message. %s', 'ok');
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.HttpError);
-    assert.ok(error instanceof erx.HttpServerError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof HttpError);
+    assert.ok(error instanceof HttpServerError);
     assert.equal(error.name, 'HttpServerError');
     assert.equal(error.status, 500);
-    done();
   });
 
-  it('should create ValidateError', function(done) {
-    var error = erx.ValidateError('Error message. %s', 'ok');
+  it('should create ValidateError', function() {
+    const error = new ValidateError('Error message. %s', 'ok');
     assert.equal(error.message, 'Error message. ok');
     assert.ok(error instanceof Error);
-    assert.ok(error instanceof erx.ErrorEx);
-    assert.ok(error instanceof erx.ValidateError);
+    assert.ok(error instanceof ErrorEx);
+    assert.ok(error instanceof ValidateError);
     assert.equal(error.name, 'ValidateError');
-    done();
   });
 
 });
